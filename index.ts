b7302b0eff25ebbbd37ba10eca3f4bfba6dfe5cb
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-03-31 15:04:29 +0500

    Update index.ts
    add 2 more new funtions

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "FirstNumber" },
-  { message: "Enter second number", type: "number", name: "SecondNumber" },
+  { message: "Enter Your first number", type: "number", name: "FirstNumber" },
+  { message: "Enter Your second number", type: "number", name: "SecondNumber" },
   {
     message: "Select one of the operators to prform operation",
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division","Square","Cube"],
   },
 ]);
 
@@ -29,7 +29,37 @@ else if(answer.operator ==="Multiplication"){
 else if(answer.operator ==="Division"){
     console.log(answer.FirstNumber / answer.SecondNumber);
 }
+if (answer.operator === "Square") {
+    var number = answer.FirstNumber; 
+    var exponent = answer.SecondNumber; 
+    var result = 1;
+    for (var i = 0; i < exponent; i++) {
+        result *= number;
+    }
+    console.log(" The square of " + exponent + " is"+ " = "+ result);
+}
+
+
+if (answer.operator === "Cube") {
+    var number = answer.FirstNumber;
+    var exponent = answer.SecondNumber; // Ensure 'exponent' is consistently typed
+    var result = 1;
+    for (var i = 0; i < exponent; i++) {
+        result *= number;
+    }
+    console.log("The cube of " + number + " is = " + result);
+}
+
+
 
 else{
     console.log("Please select valid operator");
 }
+
+
+
+
+
+
+
+
