646a58e94eebb352bfd0bb2de8dd9c6e0eaf2c83
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-04-02 08:23:50 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,65 +1,39 @@
 #!/usr/bin/env node
-
 import inquirer from "inquirer";
-
 const answer = await inquirer.prompt([
-  { message: "Enter Your first number", type: "number", name: "FirstNumber" },
-  { message: "Enter Your second number", type: "number", name: "SecondNumber" },
-  {
-    message: "Select one of the operators to prform operation",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division","Square","Cube"],
-  },
+    { message: "Enter Your first number", type: "number", name: "FirstNumber" },
+    { message: "Enter Your second number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Square", "Cube"],
+    },
 ]);
-
-
-
-if (answer.operator ==="Addition") {
+if (answer.operator === "Addition") {
     console.log(answer.FirstNumber + answer.SecondNumber);
 }
-else if(answer.operator ==="Subtraction"){
+else if (answer.operator === "Subtraction") {
     console.log(answer.FirstNumber - answer.SecondNumber);
 }
-
-else if(answer.operator ==="Multiplication"){
+else if (answer.operator === "Multiplication") {
     console.log(answer.FirstNumber * answer.SecondNumber);
 }
-
-else if(answer.operator ==="Division"){
+else if (answer.operator === "Division") {
     console.log(answer.FirstNumber / answer.SecondNumber);
 }
-if (answer.operator === "Square") {
-    var number = answer.FirstNumber; 
-    var exponent = answer.SecondNumber; 
-    var result = 1;
-    for (var i = 0; i < exponent; i++) {
-        result *= number;
-    }
-    console.log(" The square of " + exponent + " is"+ " = "+ result);
-}
-
-
-if (answer.operator === "Cube") {
+else if (answer.operator === "Square") {
     var number = answer.FirstNumber;
-    var exponent = answer.SecondNumber; // Ensure 'exponent' is consistently typed
-    var result = 1;
-    for (var i = 0; i < exponent; i++) {
-        result *= number;
-    }
+    var exponent = 2;
+    var result = Math.pow(number, exponent);
+    console.log("The square of " + number + " is = " + result);
+}
+else if (answer.operator === "Cube") {
+    var number = answer.FirstNumber;
+    var exponent = 3;
+    var result = Math.pow(number, exponent);
     console.log("The cube of " + number + " is = " + result);
 }
-
-
-
-else{
-    console.log("Please select valid operator");
+else {
+    console.log("Enter a valid operator");
 }
-
-
-
-
-
-
-
-
